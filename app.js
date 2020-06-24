eb4f39f955ac042c14295c740a74e20881d6771a
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-06-23 23:43:27 -0400

    server setup, db connect, done.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
 
 // MIDDLEWARE
+// bodyParser run every time we hit a request
+app.use(bodyParser.json());
+
 // IMPORT ROUTES
+const entriesRoute = require('./routes/entries');
+app.use('/entries', entriesRoute);
+
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Mah Home page!')
+    res.send('Home page')
 })
 
 // Connect to DB
-
+mongoose.connect(
+    process.env.DB_CONNECTION, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => console.log('connected to DB!')
+);
 
 // Listen to server
 app.listen(3000);
